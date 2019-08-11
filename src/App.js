efe884e71ef2abefd5058080cecf92da0d10cec2
DarekR88 <dradmclad@gmail.com>
2019-08-10 22:27:44 -0500

    dispatching actions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement} from './actions';
 
 function App() {
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+  const dispatch = useDispatch();
   return (
     <div className="App">
+      <h1>counter: {counter}</h1>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+
+
+      {isLogged ? <h1>valuable information I shouldn't see if I'm not logged in.</h1>: ''}
     </div>
   );
 }
